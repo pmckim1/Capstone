8863cbe13fbf487e99f7a747f584dbd39563deb4
EC2 Default User <ec2-user@ip-10-0-0-101.ec2.internal>
2021-03-31 23:45:32 +0000

    No prompts, add tmux

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@ mkdir -p WhooshIndex
 mkdir -p Cache
 mkdir -p Output
 
-sudo yum install python3 python3-devel
-sudo yum group install "Development Tools"
+sudo yum install -y tmux git
+sudo yum install -y python3 python3-devel
+sudo yum group install -y "Development Tools"
 sudo python3 -m pip install -r requirements.txt
 
