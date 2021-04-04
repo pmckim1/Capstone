6df3b092fb58d00d1ce78ac1dc9595b2a910d1f7
EC2 Default User <ec2-user@ip-172-31-64-54.ec2.internal>
2021-04-04 23:07:52 +0000

    None type check

--- 20LabelIgraph.py
+++ 20LabelIgraph.py
@@ -91,8 +91,13 @@ with ix.searcher() as s:
     for node in tqdm(i.vs):
         # Generate list of community dicts, whose cluster matches (or is a
         # sub-cluster of) the node given
-#        pprint(node)
-        salient_communities = [v for k, v in c.items() if k is not None and k.startswith(node['hier_infomap_comm'])]
+        # pprint(node)
+        hier_infomap_comm_value = node['hier_infomap_comm']
+        salient_communities = [v for k, v in c.items() if
+                               k is not None and
+                               hier_infomap_comm_value is not None
+                               and k.startswith(hier_infomap_comm_value)
+                               ]
 
         salient_articles = {}
         for d in salient_communities:
@@ -126,4 +131,5 @@ print("Dumping igraph files...")
 i.write_pickle(igraph_out)
 i.write_graphml(igraphml_out)
 print("...done.")
+
 
